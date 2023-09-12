2ee63f3717ccd2838052d1d8668a42270908b053
Joao Oliveira <j.oliveiradg@gmail.com>
2023-09-11 21:15:18 -0300

    feat: criando area adm e refactor models

--- livro/models.py
+++ livro/models.py
@@ -1,17 +1,32 @@
 from django.db import models
+from datetime import date
 
 class Livros(models.Model) :
   titulo = models.CharField(max_length = 100)
   autor = models.CharField(max_length = 40)
-  co_autor = models.CharField(max_length = 40)
-  volume = models.CharField(max_length = 10)
+  co_autor = models.CharField(max_length = 40, blank= True)
+  volume = models.CharField(max_length = 10, blank=True, null=True)
   genero = models.CharField(max_length = 100)
-  data_cadastro = models.DateField()
+  data_cadastro = models.DateField(default=date.today)
   emprestado = models.BooleanField(default=False)
-  nome_emprestado = models.CharField(max_length = 100)
-  data_emprestimo = models.DateTimeField()
-  data_devolucao = models.DateField()
+  nome_emprestado = models.CharField(max_length = 100, blank= True, null=True)
+  data_emprestimo = models.DateTimeField(blank= True, null=True)
+  data_devolucao = models.DateTimeField(blank= True, null=True)
+  tempo_duracao = models.DateField( blank= True, null=True)
   
-  class Meta:
-    verbose_name="Livro"
+  class Meta :
+    
+    verbose_name = 'Livro'
+  
+  def __str__(self) :
+    return self.titulo
+  
+  
+  
+    
+  
+  
+  
+  
+  
   
