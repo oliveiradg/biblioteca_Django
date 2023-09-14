772317f22d725a52f11a296f6be9fbc36bfc899d
Joao Oliveira <j.oliveiradg@gmail.com>
2023-09-13 23:32:09 -0300

    feat: criando static page login/cadastro

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('livro/', include('livro.urls')),
+    path('auth/', include('usuarios.urls'))
 ]
