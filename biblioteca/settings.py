e4c84f203fd9f49246f9c07019eaec32cd9320b3
Joao Oliveira <j.oliveiradg@gmail.com>
2023-09-16 09:45:01 -0300

    feat: config  key

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -14,7 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
+SECRET_KEY = 'django-insecure-_63yt2dkm#408k8*$eg^pjf&3gp-c1o4szt!c)ic*1264r7+^h'
+
+# str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
