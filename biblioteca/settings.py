defc97462e8e79ec7a3818d534abad75c23e7c70
Joao Oliveira <j.oliveiradg@gmail.com>
2023-09-16 23:03:02 -0300

    feat: criando validação cadastro

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -14,14 +14,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_63yt2dkm#408k8*$eg^pjf&3gp-c1o4szt!c)ic*1264r7+^h'
-
-# str(os.getenv('SECRET_KEY'))
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,20 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Usuario
+from django.shortcuts import redirect
+from hashlib import sha256
 
 def login(request) :
   return render(request, 'login.html')
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+  status = request.GET.get('status')
+  return render(request, 'cadastro.html', {'status: status'})
 
 def valida_cadastro(request) :
   nome = request.POST.get('nome')
   senha = request.POST.get('senha')
   email = request.POST.get('email')
   
-  usuario = Usuario(nome = nome, senha = senha, email = email)
-  usuario.save()
+   
+  usuario = Usuario.objects.filter(email = email)
+  
+  if len (nome.strip()) == 0 or len(email.strip()) ==0:
+    return redirect('auth/Cadastro/?status=1')
+  
+  if len(senha) < 8:
+    return redirect('auth/Cadastro/?status=2')
+  
+  if len(usuario) > 0 :
+    return redirect('auth/Cadastro/?status=3')
+  
+  try:
+     senha = sha256(senha.encode()).hexdigest()
+     usuario = Usuario(nome = nome, senha = senha, email = email)
+     usuario.save()
+     
+     return redirect('auth/Cadastro/?status=0')
+  
+  except:
+    return redirect('auth/Cadastro/?status=4') 
+  
+  
   
   
   
